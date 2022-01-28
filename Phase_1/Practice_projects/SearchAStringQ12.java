56c7be7134118d1e9af6ccf4bfdd0fd12a2df16c
ShubhamSapkal100 <97727402+ShubhamSapkal100@users.noreply.github.com>
2022-01-28 13:21:56 +0530

    SL

---
+++ Phase_1/Practice_projects/SearchAStringQ12.java
@@ -0,0 +1,41 @@
+package Examples;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class SearchAStringQ12 {
+	 public static boolean isValidEmail(String email) {
+		   String regex = "^(.+)@(.+)$";
+		 //initialize the Pattern object
+	       Pattern pattern = Pattern.compile(regex);
+	       Matcher matcher = pattern.matcher(email);
+	       return matcher.matches();
+
+	       
+	   }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<String> emails = new ArrayList<String>();
+	       // valid email addresses
+	       emails.add("ShubhamSapkal@example.com");
+	       emails.add("shubsapkal@example.com");
+	       emails.add("rushi@example.me.org");
+	       //invalid email addresses
+	       emails.add("Vishal.example.com");
+	       emails.add("supekar.bob@example.com");
+	       emails.add("Akash@.example.com");
+
+	       for (String value : emails) {
+	           System.out.println("The Email address " + value + " is " + (isValidEmail(value) ? "valid" : "invalid"));
+	       }
+	       
+	       System.out.println("Enter any email address to check");
+	       Scanner sc = new Scanner(System.in);
+	       String input = sc.nextLine();
+	       System.out.println("The Email address " + input + " is " + (isValidEmail(input) ? "valid" : "invalid"));
+	       
+	}
+
+}
